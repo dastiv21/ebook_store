6e9ebf885ae2b7fb000a05d1c1451d321a709f29
ayusuf@afexnigeria.com <ayusuf@afexnigeria.com>
2024-11-13 09:21:12 +0100

    testing

--- run_linters.py
+++ run_linters.py
@@ -8,26 +8,26 @@ def run_linters():
     log_filename = f"linter_log_{timestamp}.txt"
     print("Running Linters")
 
-    # with open(log_filename, 'w') as log_file:
-    #     print("Running Pylint...")
-    #     pylint_result = subprocess.run(["pylint", "."], capture_output=True,
-    #                                    text=True)
-    #     log_file.write("Pylint Output:\n")
-    #     log_file.write(pylint_result.stdout)
-    #     if pylint_result.stderr:
-    #         log_file.write("Pylint Errors:\n")
-    #         log_file.write(pylint_result.stderr)
-    #
-    #     print("\nRunning Flake8...")
-    #     flake8_result = subprocess.run(["flake8", "."], capture_output=True,
-    #                                    text=True)
-    #     log_file.write("\nFlake8 Output:\n")
-    #     log_file.write(flake8_result.stdout)
-    #     if flake8_result.stderr:
-    #         log_file.write("\nFlake8 Errors:\n")
-    #         log_file.write(flake8_result.stderr)
-    #
-    #     print("\nLinter checks completed.")
+    with open(log_filename, 'w') as log_file:
+        print("Running Pylint...")
+        pylint_result = subprocess.run(["pylint", "**/*.py"], capture_output=True,
+                                       text=True)
+        log_file.write("Pylint Output:\n")
+        log_file.write(pylint_result.stdout)
+        if pylint_result.stderr:
+            log_file.write("Pylint Errors:\n")
+            log_file.write(pylint_result.stderr)
+
+        print("\nRunning Flake8...")
+        flake8_result = subprocess.run(["flake8", "."], capture_output=True,
+                                       text=True)
+        log_file.write("\nFlake8 Output:\n")
+        log_file.write(flake8_result.stdout)
+        if flake8_result.stderr:
+            log_file.write("\nFlake8 Errors:\n")
+            log_file.write(flake8_result.stderr)
+
+        print("\nLinter checks completed.")
 
 
 # Call the function
